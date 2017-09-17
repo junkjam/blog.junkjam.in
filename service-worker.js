33b12d09a0e020820aac8ec282a4a0fccd350fc5
Fasih Ahmad Fakhri <fasihahmadfakhri@gmail.com>
2017-09-18 01:11:22 +0530

    Site updated at 2017-09-17 19:41:22 UTC

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,31 @@
 importScripts('/cache-polyfill.js');
+
+var version = 'v1.0';
+var cacheNameSpace = 'JunkJamBlog';
+var cacheName = cacheNameSpace + '-' + version;
+
 self.addEventListener('install', function(e) {
-e.waitUntil(
-caches.open('junkjam').then(function(cache) {
-return cache.addAll([
-'/',
-'/index.html',
-'/stylesheets/screen.css',
-'/javascripts/octopress.js',
-]);
-})
-);
+    e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+        return cache.addAll([
+            '/',
+            '/categories/market/',
+            '/categories/fashion/',
+            '/categories/lifestyle/',
+            '/stylesheets/screen.css?v=4.0',
+            '/javascripts/octopress.js?v=4.0',
+        ]);
+    })
+    );
 });
 
 
 self.addEventListener('fetch', function(event) {
-console.log(event.request.url);
-event.respondWith(
-caches.match(event.request).then(function(response) {
-return response || fetch(event.request);
-})
-);
+    //console.log(event.request.url);
+    event.respondWith(
+    caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+    })
+    );
 });
 
