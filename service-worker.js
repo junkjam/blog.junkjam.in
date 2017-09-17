46b1242383076a63ebdd033c982ab5a76dba1f59
Fasih Ahmad Fakhri <fasihahmadfakhri@gmail.com>
2017-09-17 18:21:56 +0530

    Site updated at 2017-09-17 12:51:56 UTC

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+importScripts('/cache-polyfill.js');
+self.addEventListener('install', function(e) {
+e.waitUntil(
+caches.open('junkjam').then(function(cache) {
+return cache.addAll([
+'/',
+'/index.html',
+'/stylesheets/screen.css',
+'/javascripts/octopress.js',
+]);
+})
+);
+});
+
+
+self.addEventListener('fetch', function(event) {
+console.log(event.request.url);
+event.respondWith(
+caches.match(event.request).then(function(response) {
+return response || fetch(event.request);
+})
+);
+});
+
